0ff1c5ad1465305b680a129a92e472551809201f
matteodallo <matteo.dallora@mail.polimi.it>
2023-07-31 17:29:25 +0200

    My paths

--- Codici/Optimization/setPath.m
+++ Codici/Optimization/setPath.m
@@ -12,10 +12,13 @@ addpath("XFOIL")
 % SU2_RUNS add
 setenv('PATH', [getenv('PATH') ';\\wsl.localhost\Ubuntu\home\fede\Codes\SU2_bin\bin'])
 % setenv('PATH', [getenv('PATH') ';\\wsl.localhost\Ubuntu\home\matteo\codes\su2\SU2_bin\bin'])
+% setenv('PATH', [getenv('PATH') ';\\wsl.localhost\Ubuntu\home\matteodallo\cfd\SU2_bin\bin'])
 
 % gmsh add
 setenv('PATH', [getenv('PATH') ';\\wsl.localhost\Ubuntu\home\fede\Codes\gmsh-4.10.5-Linux64\bin'])
 %setenv('PATH', [getenv('PATH') ';\\wsl.localhost\Ubuntu\home\matteo\codes\gmsh\gmsh-4.10.5-Linux64\bin'])
+%setenv('PATH', [getenv('PATH') ';\\wsl.localhost\Ubuntu\home\matteodallo\cfd\gmsh-4.10.5-Linux64\bin'])
+
 
 % check path
 !wsl env
